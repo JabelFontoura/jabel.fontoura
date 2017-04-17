8cab655e85ff544da012190a7c9a977db9290a56
JabelCwi <jabel.fontoura@cwi.com.br>
2017-04-17 20:22:06 -0300

    feat(Golpe): Implementacao inicial de golpe

---
+++ Modulo1-Cavaleiros/Golpe.java
@@ -0,0 +1,10 @@
+public class Golpe {
+ 
+    private String nome;
+    private int fatorDano;
+    
+    public Golpe(String nome, int fatoDano) {
+        this.nome = nome;
+        this.fatorDano = fatorDano;
+    } 
+}
