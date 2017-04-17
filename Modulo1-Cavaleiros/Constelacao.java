872975c0e4d8280f519d3d5df2d9c232722742e1
JabelCwi <jabel.fontoura@cwi.com.br>
2017-04-17 20:22:37 -0300

    feat(Constelacao): Implementacao inicial de Constelacao

---
+++ Modulo1-Cavaleiros/Constelacao.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Constelacao {
+    
+    private String nome;
+    private List<Golpe> golpes; 
+    
+    public Constelacao(String nome) {
+        this.nome = nome;        
+        this.golpes = new ArrayList<Golpe>();
+    }
+    
+    public void adicionarGolpe(Golpe golpe) {
+        golpes.add(golpe);
+    }
+    
+    public List<Golpe> getGolpes() {
+        return this.golpes;
+    }
+}
