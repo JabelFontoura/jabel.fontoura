accb409b7e0b25e44676188c03bc70c5d2302333
Jabel <jabel.fontoura@cwi.com.br>
2017-04-17 15:10:34 -0400

    test(Batalha): integridade da lógica de iniciaBatalha()

---
+++ Modulo1-Cavaleiros/BatalhaTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BatalhaTest {
+
+    @Test
+    public void categoriaOponenteMaiorQueDesafiador() {
+        Saint shaina = new Saint("Shaina", new Armadura("Serpente", Categoria.PRATA));
+        Saint hyoga = new Saint("Hyoga", new Armadura("Serpente", Categoria.BRONZE));
+
+        Batalha batalha = new Batalha(shaina, hyoga);
+
+        batalha.iniciaBatalha();
+
+        assertEquals(100, shaina.getVida(), 0.01);
+        assertEquals(90, hyoga.getVida(), 0.01);
+    }
+
+    @Test
+    public void categoriasIguaisDesafiadorDevePerderVida() {
+        Saint aldebaram = new Saint("Aldebaram", new Armadura("Aldebaram", Categoria.OURO));
+        Saint mascaraMorte = new Saint("Mascara da Morte", new Armadura("Cancer", Categoria.OURO));
+
+        Batalha batalha = new Batalha(aldebaram, mascaraMorte);
+
+        batalha.iniciaBatalha();
+
+        assertEquals(100, aldebaram.getVida(), 0.01);
+        assertEquals(90, mascaraMorte.getVida(), 0.01);
+    }
+    
+      @Test
+     public void categoriaDesafiadorMaiorOponentePerdeVida() {
+         // Arrange
+         Saint ikki = new Saint("Ikki", new Armadura("Fênix", Categoria.BRONZE));
+         Saint mascaraMorte = new Saint("Máscara da Morte", new Armadura("Câncer", Categoria.OURO));
+         Batalha batalha = new Batalha(ikki, mascaraMorte);
+         // Act
+         batalha.iniciaBatalha();
+         // Assert
+         assertEquals(90, ikki.getVida(), 0.01);
+         assertEquals(100, mascaraMorte.getVida(), 0.01);
+     }
+}
