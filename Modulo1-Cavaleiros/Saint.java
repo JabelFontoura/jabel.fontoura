38895c7a311977026bebe844f8cff1a23d40497a
JabelCwi <jabel.fontoura@cwi.com.br>
2017-04-17 19:47:02 -0300

    refactor(Saint): validar status e dano ao perderVida

--- Modulo1-Cavaleiros/Saint.java
+++ Modulo1-Cavaleiros/Saint.java
@@ -1,6 +1,7 @@
+import java.security.InvalidParameterException;
 
 public class Saint {
-    
+
     private String nome;
     private Armadura armadura;
     private boolean armaduraVestida = false;
@@ -8,61 +9,66 @@ public class Saint {
     private Status status = Status.VIVO;
     private double vida = 100.0;
     protected int qtdSentidosDespertados;
-    
+
     public Saint(String nome, Armadura armadura) throws Exception {
         this.nome = nome;
         this.armadura = armadura;
         this.genero = genero;
-        
-       // this.qtdSentidosDespertados += getValorCategoria();
+
+        // this.qtdSentidosDespertados += getValorCategoria();
     }
-    
+
     public void vestirArmadura() {
         this.armaduraVestida = true;
     }
-    
+
     public void perderVida(double dano) {
-        this.vida -= dano;
-        
-        this.status = atualizaStatus();
+        if(dano < 0) {
+            throw new InvalidParameterException("O valor do dano não pode ser menor que 0.");
+        }
+
+        if(status != Status.MORTO) {  
+            this.vida -= dano;
+            this.status = atualizaStatus();
+        }
     }
-    
+
     private Status atualizaStatus() {
-        if(this.vida == 0) {
+        if(this.vida == 1) {
             return Status.DESACORDADO;
-        }else if(this.vida < 0) {
+        }else if(this.vida < 1) {
             return Status.MORTO;
         }
-        
+
         return this.status.VIVO;
     }
-    
+
     public int getValorCategoria() {
         return this.armadura.getValorCategoria();
     }
-    
+
     public boolean isArmaduraVestida() {
         return this.armaduraVestida;
     }
-    
+
     public Genero getGenero() {
         return this.genero;
     }
-    
+
     public void setGenero(Genero genero) {
         this.genero = genero;
     }
-    
+
     public Status getStatus(){
         return this.status;
     }
-    
+
     public double getVida() {
         return this.vida;
     }
-    
+
     public int getqtdSentidosDespertados() {
         return this.qtdSentidosDespertados;
     }
-    
+
 }
