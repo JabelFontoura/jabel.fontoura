42011684ae828240a951a9877107f71430477ef6
JabelCwi <jabel.fontoura@cwi.com.br>
2017-04-19 01:06:44 -0300

    feat(ListaSaints): implementar restante dos metodos

--- Modulo1-Cavaleiros/ListaSaints.java
+++ Modulo1-Cavaleiros/ListaSaints.java
@@ -1,30 +1,32 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ListaSaints {
-    
+
     private List<Saint> lista = new ArrayList<Saint>();
-    
+
     public ListaSaints() {
-        
+
     }
-    
+
     public void adicionar(Saint saint) {
         lista.add(saint);
     }
-    
+
     public Saint get(int i) {
         return lista.get(i);
     }
-    
+
     public List<Saint> todos() {
         return lista;
     }
-    
+
     public void remover(Saint saint) {
         lista.remove(saint);
     }
-    
+
     public Saint buscarPorNome(String nome) {
         for(int i = 0; i < lista.size(); i++) {
             if(nome.equals(lista.get(i).getNome())) {
@@ -33,25 +35,62 @@ public class ListaSaints {
         }
         return null;
     }
-    
-    public Saint buscarPorCategoria(Categoria categoria) {
+
+    public List<Saint> buscarPorCategoria(Categoria categoria) {
         List<Saint> listaCategoria = new ArrayList<Saint>();
-        
+
         for(int i = 0; i < lista.size(); i++) {
             if(categoria.getValor() == lista.get(i).getValorCategoria()) {
-                return lista.get(i);
+                listaCategoria.add(lista.get(i));
             }
         }
-        return null;
+        return listaCategoria;
     }
-    
-    public Saint buscarPorStatus(Status status) {
+
+    public List<Saint >buscarPorStatus(Status status) {
+        List<Saint> listaStatus = new ArrayList<Saint>();
+
         for(int i = 0; i < lista.size(); i++) {
             if(status == lista.get(i).getStatus()) {
-                return lista.get(i);
+                listaStatus.add(lista.get(i));
             }
         }
-        return null;
+        return listaStatus;
     }
-    
+
+    public Saint getSaintMaiorVida() throws Exception {
+        Saint saintComMaiorVida = new Saint("Seiya", new Armadura(new Constelacao("Pegaso"), Categoria.BRONZE));
+        saintComMaiorVida.perderVida(99);
+
+        for(Saint saint : lista) {
+            if(saint.getVida() > saintComMaiorVida.getVida()) {
+                saintComMaiorVida = saint;
+            }
+        }
+
+        return saintComMaiorVida;       
+    }
+
+    public Saint getSaintMenorVida() {
+        ordenar();
+        return lista.get(0);
+    }
+
+    public void ordenar() { // Tava sem tempo      
+        //Collections.sort(lista, (saint1, saint2) -> Double.compare(saint1.getVida(), saint2.getVida()));
+       
+       int j;
+       Saint aux;
+       
+       for (int i = 1; i < lista.size(); i++) {    
+          aux = lista.get(i);
+          
+          for (j = i - 1; (j >= 0) && (lista.get(j).getVida() > aux.getVida()); j--) {
+              lista.set(j + 1, lista.get(j));
+          }
+          lista.set(j + 1, aux);
+        }
+       
+   }
+
 }
--- Modulo1-Cavaleiros/ListaSaintsTest.java
+++ Modulo1-Cavaleiros/ListaSaintsTest.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ListaSaintsTest {
     
@@ -26,12 +28,69 @@ public class ListaSaintsTest {
     }
     
     @Test
-    public void buscarSaintPorCategoriaBronzeDeveRetornarSaintCerto() throws Exception {
+    public void buscarSaintPorCategoriaBronzeDeveRetornarListaDeSaintsCerto() throws Exception {
         Saint seiya = new Saint("Seiya", new Armadura(new Constelacao("Pegaso"), Categoria.BRONZE));
         ListaSaints lista = new ListaSaints();
+        Saint marin = new Saint("Marin", new Armadura(new Constelacao("Aguia"), Categoria.PRATA));
         
         lista.adicionar(seiya);
+        lista.adicionar(marin);
         
-        assertEquals(seiya, lista.buscarPorCategoria(Categoria.BRONZE));
+        List<Saint> listaCategoria = new ArrayList<Saint>();
+        
+        listaCategoria.add(seiya);
+        
+        assertEquals(listaCategoria, lista.buscarPorCategoria(Categoria.BRONZE));
     }
+    
+    @Test
+    public void getSaintMenorVidaDeFatoRetornaOComMenorVida() throws Exception {
+        ListaSaints lista = new ListaSaints();
+        Saint seiya = new Saint("Seiya", new Armadura(new Constelacao("Pegaso"), Categoria.BRONZE));
+        Saint marin = new Saint("Marin", new Armadura(new Constelacao("Aguia"), Categoria.PRATA));
+        Saint saint1 = new Saint("Seiya", new Armadura(new Constelacao("Pegaso"), Categoria.BRONZE));
+        Saint saint2 = new Saint("Seiya", new Armadura(new Constelacao("Pegaso"), Categoria.BRONZE));
+        Saint saint3 = new Saint("Seiya", new Armadura(new Constelacao("Pegaso"), Categoria.BRONZE));
+        
+        marin.perderVida(90);
+        saint1.perderVida(10);
+        saint2.perderVida(40);
+        
+        lista.adicionar(seiya);
+        lista.adicionar(marin);
+        lista.adicionar(saint1);
+        lista.adicionar(saint2);
+        lista.adicionar(saint3);
+        
+        assertEquals(marin, lista.getSaintMenorVida());
+        
+    }
+    
+    @Test
+    public void ordernarDeveSerAscendente() throws Exception {
+        ListaSaints lista = new ListaSaints();
+        Saint seiya = new Saint("Seiya", new Armadura(new Constelacao("Pegaso"), Categoria.BRONZE));
+        Saint marin = new Saint("Marin", new Armadura(new Constelacao("Aguia"), Categoria.PRATA));
+        Saint saint1 = new Saint("Seiya", new Armadura(new Constelacao("Pegaso"), Categoria.BRONZE));
+        Saint saint2 = new Saint("Seiya", new Armadura(new Constelacao("Pegaso"), Categoria.BRONZE));
+        Saint saint3 = new Saint("Seiya", new Armadura(new Constelacao("Pegaso"), Categoria.BRONZE));
+        
+        marin.perderVida(90);
+        saint1.perderVida(10);
+        saint2.perderVida(40);
+        
+        lista.adicionar(seiya);
+        lista.adicionar(marin);
+        lista.adicionar(saint1);
+        lista.adicionar(saint2);
+        lista.adicionar(saint3);
+        
+        lista.ordenar();
+        
+        for(int i = 0; i < lista.todos().size() - 1; i++) {
+            System.out.println(lista.get(i).getVida());
+            assertTrue(lista.get(i).getVida() <= lista.get(i + 1).getVida()); 
+        }
+    }
+    
 }
