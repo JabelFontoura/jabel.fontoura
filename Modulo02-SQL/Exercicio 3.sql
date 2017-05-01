e2dc0e95cdb22d1f2bc8c37a27dee153ce857d54
JabelCwi <jabel.fontoura@cwi.com.br>
2017-05-01 11:11:08 -0300

    feat(Exercicio 3):  subir novamente comandos que nao foram para o git

--- Modulo02-SQL/Exercicio 3.sql
+++ Modulo02-SQL/Exercicio 3.sql
@@ -3,3 +3,16 @@
 -- 1)
 SELECT IDEmpregado, NomeEmpregado, DATEDIFF(year, DataNascimento, DataAdmissao) AS IdadeAdmissao
 FROM Empregado WHERE YEAR(DataAdmissao) = 1980; 
+
+-- 2)
+SELECT TOP 1 Cargo FROM Empregado
+GROUP BY Cargo ORDER BY COUNT(Cargo) DESC;
+
+-- 3)
+SELECT UF, COUNT(Nome) AS TotalDeCidades 
+FROM Cidade GROUP BY UF;
+
+-- 4)
+ INSERT INTO Departamento VALUES (70, 'Inovação', 'SÃO LEOPOLDO');
+
+ UPDATE Empregado SET IDDepartamento = 70 WHERE MONTH(DataAdmissao) = 12 AND Cargo != 'Atendente';
