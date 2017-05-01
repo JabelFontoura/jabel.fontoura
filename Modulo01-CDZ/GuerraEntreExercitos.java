7e00c36fbfc89f1a6c762e2a43f21851b4f920b5
JabelCwi <jabel.fontoura@cwi.com.br>
2017-05-01 00:50:47 -0300

    feat(GuerraEntreExercitos): implementacao inicial de classe

---
+++ Modulo01-CDZ/GuerraEntreExercitos.java
@@ -0,0 +1,18 @@
+public class GuerraEntreExercitos {
+    
+    private ExercitoDeSaints defensoresDeAthena, impostores;
+    
+    public GuerraEntreExercitos(ExercitoDeSaints defensoresDeAthena, ExercitoDeSaints impostores) {
+        this.defensoresDeAthena = defensoresDeAthena;
+        this.impostores = impostores;
+    }
+    
+    public void iniciar() { // TODO testar
+        int tamanho = impostores.getLista().todos().size() < defensoresDeAthena.getLista().todos().size() ? 
+            impostores.getLista().todos().size() : defensoresDeAthena.getLista().todos().size();
+            
+         for(int i = 0; i < tamanho; i++) {
+             new Batalha(defensoresDeAthena.getLista().get(i), impostores.getLista().get(i));
+         }
+    }
+}
