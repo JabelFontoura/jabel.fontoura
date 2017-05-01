c41ae89ce3d1ea9bb529d3d918bcf398ee67144e
JabelCwi <jabel.fontoura@cwi.com.br>
2017-05-01 11:59:19 -0300

    feat(Exercicio 3): adicionar transaction

--- Modulo02-SQL/Exercicio 3.sql
+++ Modulo02-SQL/Exercicio 3.sql
@@ -13,6 +13,11 @@ SELECT UF, COUNT(Nome) AS TotalDeCidades
 FROM Cidade GROUP BY UF;
 
 -- 4)
+BEGIN TRANSACTION
+
  INSERT INTO Departamento VALUES (70, 'Inovação', 'SÃO LEOPOLDO');
 
  UPDATE Empregado SET IDDepartamento = 70 WHERE MONTH(DataAdmissao) = 12 AND Cargo != 'Atendente';
+
+ COMMIT
+ GO
