5d4408bbc4531ab26ce9c9fd422b2508b9b74426
Jabel <jabel.fontoura@cwi.com.br>
2017-05-02 17:28:43 -0400

    feat(Exercicio 5): comandos até o 4

---
+++ Modulo-02/Exercicio 5.sql
@@ -0,0 +1,35 @@
+-- Exercicio
+
+-- 1) Liste o nome do empregado, o nome do gerente e o departamento de cada um.
+SELECT emp.NomeEmpregado, ger.NomeEmpregado AS NomeGerente, dep.NomeDepartamento
+FROM Empregado emp JOIN Empregado ger ON emp.IDGerente = ger.IDEmpregado
+JOIN Departamento dep ON emp.IDDepartamento = dep.IDDepartamento;
+
+-- 2) Liste o deparamento (id e nome) com o empregado de maior salário.
+SELECT DISTINCT dep.IDDepartamento, dep.NomeDepartamento, emp.Salario FROM Departamento dep
+JOIN Empregado emp ON dep.IDDepartamento = emp.IDDepartamento
+AND emp.Salario = (SELECT MAX(Salario) FROM Empregado WHERE IDDepartamento IS NOT NULL);
+
+-- 3) Aplique uma alteração salarial em todos os empregados que o departamento fique na localidade de SAO PAULO,
+--    este reajuste deve ser de 17,3%. Por segurança faça uma cópia da tabela Empregado antes de iniciar esta tarefa.
+SELECT * INTO CopiaEmpregado FROM Empregado;
+
+BEGIN TRANSACTION
+	UPDATE emp SET emp.Salario = emp.Salario * 1.173 FROM Empregado emp
+	JOIN Departamento dep ON emp.IDDepartamento = dep.IDDepartamento AND dep.Localizacao IN ('SAO PAULO');
+GO
+COMMIT
+
+-- 4) Liste todas as cidades duplicadas (nome e UF iguais).	
+SELECT Nome, COUNT(Nome) FROM Cidade
+GROUP BY Nome HAVING COUNT(Nome) > 1;
+
+-- 5) Faça uma alteraçao nas cidades que tenham nome+UF duplicados, adicione no final do nome um asterisco. 
+--    Mas atenção! apenas a cidade com maior ID deve ser alterada.
+BEGIN TRANSACTION
+UPDATE c SET c.Nome = c.Nome + '*' FROM (
+	SELECT Nome, COUNT(Nome) FROM Cidade
+	GROUP BY Nome HAVING COUNT(Nome) > 1
+) c;
+GO
+COMMIT
