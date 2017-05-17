d27b909590e118e99c4082c6af259ef02fad7581
Jabel <jabel.fontoura@cwi.com.br>
2017-05-17 17:33:07 -0400

    feat(exercicio2): implementacao do controller, repeat e filter

--- Modulo05-Angular/aula01/js/aula01.js
+++ Modulo05-Angular/aula01/js/aula01.js
@@ -1,8 +1,32 @@
 const module = angular.module('aula01', []);
 
-module.controller('Exemplo1', $scope => {
+module.controller('Exercicio1', $scope => {
     $scope.pokemon = {
         nome: 'Pikachu',
         tipo: 'Elétrico'
     }
+});
+module.controller('Exercicio2', $scope => {
+    $scope.pokemons = [
+        { 
+					nome: 'Pikachu',
+					tipo: 'Elétrico'
+        }, 
+        { 
+					nome: 'Bulbasauro',
+					tipo: 'Grama'
+        },
+        { 
+					nome: 'Charmander',
+					tipo: 'Fogo'
+        },
+        { 
+					nome: 'Squirtle',
+					tipo: 'Água'
+        },
+				{ 
+					nome: 'Blastoise',
+					tipo: 'Água'
+        }
+       ];
 });
