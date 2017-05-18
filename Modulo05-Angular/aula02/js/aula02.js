e400b6879cf6e2def51e8b859f0312832fcb19eb
Jabel <jabel.fontoura@cwi.com.br>
2017-05-18 16:18:04 -0400

    refactor(aula02): nao aceitar valores invalidos

--- Modulo05-Angular/aula02/js/aula02.js
+++ Modulo05-Angular/aula02/js/aula02.js
@@ -2,6 +2,9 @@ const module = angular.module('aula02', []);
 
 module.controller('Exercicio2', ($scope) => {
     $scope.imprime = () => {
+        if(angular.isUndefined($scope.data)) 
+            return;
+            
         const partesData = $scope.data.split("/");
         $scope.resultadoData = new Date(partesData[2], partesData[1] - 1, partesData[0]);
     }
