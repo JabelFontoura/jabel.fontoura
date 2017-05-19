87f16b595f0d0f2da54f89ce906aa78f64b3617d
JabelCwi <jabel.fontoura@cwi.com.br>
2017-05-18 21:37:52 -0300

    feat(tema): exercicio 1

--- Modulo05-Angular/aula02/js/aula02.js
+++ Modulo05-Angular/aula02/js/aula02.js
@@ -10,43 +10,20 @@ module.controller('Exercicio2', ($scope) => {
 });
 
 module.controller('Exercicio3', ($scope, $filter) => {
-
-    const instrutores = [{
-        nome: 'Bernardo',
-        aula: [{
-            numero: 1,
-            nome: 'OO'
-        },
-        {
-            numero: 4,
-            nome: 'Javascript'
-        }
-        ]
-    },
-    {
-        nome: 'Nunes',
-        aula: [{
-        numero: 2,
-        nome: 'Banco de Dados I'
-        }]
-    },
-    {
-        nome: 'Pedro (PHP)',
-        aula: [{
-        numero: 3,
-        nome: 'HTML e CSS'
-        }]
-    },
-    {
-        nome: 'Zanatta',
-        aula: [{
-        numero: 5,
-        nome: 'AngularJS'
-        }]
-    }
-    ];
-    
     $scope.instrutores = instrutores;
 });
 
+module.controller('Tema1', ($scope, $filter) => {
+	let aulas = [];
+
+	instrutores.forEach(item => {
+		item.aula.forEach(e => aulas.push({numero: e.numero, aula: e.nome, instrutor: item.nome }));
+	});
+
+	$scope.aulasInstrutor = aulas.sort((a, b) => a.numero - b.numero);
+
+});
+
+//FILTERS
 module.filter('mascada', () => (nome) => nome.replace(/(nunes)/i, '$ $1 $'));
+
---
+++ Modulo05-Angular/aula02/js/lista-instrutores.js
@@ -0,0 +1,34 @@
+const instrutores = [{
+        nome: 'Bernardo',
+        aula: [{
+            numero: 1,
+            nome: 'OO'
+        },
+        {
+            numero: 4,
+            nome: 'Javascript'
+        }
+        ]
+    },
+    {
+        nome: 'Nunes',
+        aula: [{
+        numero: 2,
+        nome: 'Banco de Dados I'
+        }]
+    },
+    {
+        nome: 'Pedro (PHP)',
+        aula: [{
+        numero: 3,
+        nome: 'HTML e CSS'
+        }]
+    },
+    {
+        nome: 'Zanatta',
+        aula: [{
+        numero: 5,
+        nome: 'AngularJS'
+        }]
+    }
+    ];
