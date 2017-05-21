6f5f7f5d5e50b0ae19b282d6dfb2a94ead07b5ae
JabelCwi <jabel.fontoura@cwi.com.br>
2017-05-21 17:53:00 -0300

    feat(crud): adicionar edit e delete ao instrutor

--- Modulo05-Angular/aula03/crud/controller.js
+++ Modulo05-Angular/aula03/crud/controller.js
@@ -3,8 +3,9 @@ const module = angular.module('crud', []);
 module.controller('MainController', ($scope) => {
   let idAula = 0;
   let idInstrutor = 0;
-  $scope.aulas = [];
-  $scope.instrutores = [];
+  $scope.aulas = [{id: idAula++, nome: 'HTML & CSS'}, {id: idAula++, nome: 'Javascript'}, {id: idAula++, nome: 'Angular'}];
+  $scope.instrutores = [{id: idInstrutor++, nome:'teste', aulas: [ {id: 0, nome: 'HTML & CSS'}, {id: 1, nome: 'Javascript'}]}];
+
 
   $scope.adicionarAula = () => {
     if($scope.createAula.$valid) {
@@ -17,36 +18,41 @@ module.controller('MainController', ($scope) => {
     }
   }
 
+  $scope.mostrarAula = (item) => {
+    $scope.editAula = {};
+    $scope.showEditAula = true;
+    $scope.idAulaUpdate = item.id;
+    $scope.editAula.nome = item.nome;
+  }
+
   $scope.editarAula = () => {
     if($scope.updateAula.$valid) {
       $scope.aulas.forEach(item => $scope.editAulaExiste = item.nome === $scope.editAula.nome);
       if(!$scope.editAulaExiste){
         $scope.aulas.forEach((item) => {
-          if(item.id === Number($scope.idUpdate)) 
+          if(item.id === Number($scope.idAulaUpdate)) 
             item.nome = $scope.editAula.nome;
         });
       }
       $scope.editAula = {};
-      $scope.showEdit = false;
+      $scope.showEditAula = false;
     }
   }
 
   $scope.deletarAula = (id) => {
-    console.log(getIdIndex(id));
-      $scope.aulas.splice(getIdIndex(id), 1);
-  }
-
-  $scope.mostrarAula = (item) => {
-    $scope.editAula = {};
-    $scope.showEdit = true;
-    $scope.idUpdate = item.id;
-    $scope.editAula.nome = item.nome;
+      $scope.aulas.splice(getIdIndex(id, $scope.aulas), 1);
   }
 
   $scope.adicionarInstrutor = () => {
     if($scope.createInstrutor.$valid) {
-      $scope.aulas.forEach(item => $scope.createInstrutorExiste = item.nome === $scope.novoInstrutor.nome);
+      $scope.instrutores.forEach(item => $scope.createInstrutorExiste = item.nome === $scope.novoInstrutor.nome);
       if(!$scope.createInstrutorExiste){
+        // if(!$scope.novoInstrutor.deuAula) {
+        //   $scope.novoInstrutor.dandoAula = 'Não';
+        // } else {
+        //   $scope.novoInstrutor.dandoAula = 'Sim';
+        // } 
+
         $scope.novoInstrutor.id = idInstrutor++;
         $scope.novoInstrutor.aulas = getValorCheckbox();
         $scope.instrutores.push(angular.copy($scope.novoInstrutor));
@@ -55,8 +61,48 @@ module.controller('MainController', ($scope) => {
     }
   }
 
-  $scope.editarInstrutor = () => {
+  $scope.mostrarInstrutor = (item) => {
+    $scope.editInstrutor = {};
+    $scope.showEditInstrutor = true;
+    $scope.idInstrutorUpdate = item.id;
+    $scope.editInstrutor = Object.assign({}, item);
+    console.log($scope.editInstrutor);
+  }
 
+
+  $scope.editarInstrutor = () => {
+    if($scope.updateInstrutor.$valid) {
+      $scope.instrutores.forEach(item => $scope.editInstrutorExiste = item.nome === $scope.editInstrutor.nome);
+
+      if(!$scope.editInstrutorExiste){
+        $scope.instrutores.forEach((item) => {
+            console.log(item);
+          if(item.id === Number($scope.idInstrutorUpdate)) {
+            item.nome = $scope.editInstrutor.nome;
+            item.foto = $scope.editInstrutor.foto;
+            item.sobrenome = $scope.editInstrutor.sobrenome;
+            item.idade = $scope.editInstrutor.idade;
+            item.email = $scope.editInstrutor.email;
+            item.dandoAula = $scope.editInstrutor.dandoAula;
+          }
+        });
+      }
+      $scope.editInstrutor = {};
+      $scope.showEditInstrutor = false;
+    }
+  }
+
+  $scope.deletarInstrutor = (id) => {
+    $scope.instrutores.splice(getIdIndex(id, $scope.instrutores), 1);
+    $scope.showEditInstrutor = false;
+  }
+
+  $scope.getCheckedAula = (id) => {
+    if(!angular.isUndefined($scope.editInstrutor) && Object.keys($scope.editInstrutor).length > 0) {
+      for(aula of $scope.editInstrutor.aulas) 
+        if(aula.id === id) return true;
+    }
+      return false;
   }
 
   function getValorCheckbox() {
@@ -73,9 +119,9 @@ module.controller('MainController', ($scope) => {
     return result;
   }
 
-  function getIdIndex(id) {
-    for(let i = 0; i < $scope.aulas.length; i++) 
-        if($scope.aulas[i].id === id) return i;
+  function getIdIndex(id, array) {
+    for(let i = 0; i < array.length; i++) 
+        if(array[i].id === id) return i;
       
       return -1;
   }
