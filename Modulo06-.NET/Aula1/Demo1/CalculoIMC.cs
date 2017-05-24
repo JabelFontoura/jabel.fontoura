95384665354a34062e251168aec5f8baca1eb9b9
Jabel <jabel.fontoura@cwi.com.br>
2017-05-24 17:11:50 -0400

    feat(.NET): adicionar estrutura do modulo

---
+++ Modulo06-.NET/Aula1/Demo1/CalculoIMC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class CalculoIMC
+    {
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+
+        public CalculoIMC(double altura, double peso)
+        {
+            this.Altura = altura;
+            this.Peso= peso;
+        }
+
+        public double calcular()
+        {
+            return Peso / (Math.Pow(Altura, 2));
+        }
+    }
+}
---
+++ Modulo06-.NET/Aula1/Demo1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var inputs = new List<int>();
+            var inputs = new int[] { };
+            var input = "";
+
+            while (input != "exit")
+            {
+                Console.WriteLine("Digite um valor: ");
+                input = Console.ReadLine();
+
+                if (input == "exit") break;
+
+                //inputs.Add(int.Parse(input));
+                var auxInputs = new int[inputs.Length + 1];
+
+                for (int i = 0; i < inputs.Length; i++)
+                    auxInputs[i] = inputs[i];
+
+                auxInputs[inputs.Length] = int.Parse(input);
+
+                inputs = auxInputs;
+            }
+
+            foreach (var item in inputs)
+                Console.WriteLine("Valor: " + item);
+
+            Console.ReadKey();
+        }
+    }
+}
