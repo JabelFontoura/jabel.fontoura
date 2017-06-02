35d2ab6f96f43f049d87358fd899354a49e656f6
JabelCwi <jabel.fontoura@cwi.com.br>
2017-06-02 01:55:38 -0300

    feat(trabalho01): ajustes na paginacao

--- Modulo06-.NET/trabalho01/Front/js/controllers/livros.controller.js
+++ Modulo06-.NET/trabalho01/Front/js/controllers/livros.controller.js
@@ -1,4 +1,9 @@
 angular.module('app').controller('LivrosController', function ($scope, livrosService) {
+  let paginaAtual = 1;
+
+  livrosService.listar()
+    .then(response => gerarQuantidadePaginas(response.data.dados.length / 5))
+    .catch(error => console.log(error));
 
   $scope.parametros = {
       quantidadePular: 0,
@@ -7,15 +12,29 @@ angular.module('app').controller('LivrosController', function ($scope, livrosSer
 
   listar();
 
-  $scope.voltar = () => {
-    $scope.parametros.quantidadePular -= 5;
-    if($scope.parametros.quantidadePular < 0) $scope.parametros.quantidadePular = 0;
+  $scope.irParaPagina = (num) => {
+    let result = 0;
 
+    if(num > paginaAtual) 
+      $scope.parametros.quantidadePular += (num - 1) * 5;
+    else if(num < paginaAtual)
+      $scope.parametros.quantidadePular -= (num) * 5
+    
+    console.log('pular',$scope.parametros.quantidadePular);
+    console.log('num', num);
+    console.log('pagina', paginaAtual);
     listar();
+    paginaAtual = num;
   }
 
   $scope.avancar = () => {
     $scope.parametros.quantidadePular += 5;
+    listar();
+  }
+
+  $scope.voltar = () => {
+    $scope.parametros.quantidadePular -= 5;
+    if($scope.parametros.quantidadePular < 0) $scope.parametros.quantidadePular = 0;
     listar();
   }
   
@@ -27,5 +46,11 @@ angular.module('app').controller('LivrosController', function ($scope, livrosSer
     livrosService.listar($scope.parametros)
       .then(response => $scope.livros = response.data.dados)
       .catch(error => console.log(error));
+  }
+
+  function gerarQuantidadePaginas(num) {
+    $scope.totalLivros = [];
+    for(var i = 1; i <= Math.ceil(num); i++) 
+      $scope.totalLivros.push({indice: i}); 
   }
 });
