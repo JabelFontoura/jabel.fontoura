dbef10bfcff11e6d9412aba1b26776cc839c8a2a
Jabel <jabel.fontoura@cwi.com.br>
2017-06-12 18:06:01 -0400

    feat(exercicios): comandos do exercicio aula01

---
+++ Modulo07-Banco2/Aula01/CRESCER 2017 - Aula.sql
@@ -0,0 +1,21 @@
+-- 1) Liste os produtos (id e nome) que não tiveram nenhuma compra nos últimos quatro meses.
+SELECT prod.IDPRODUTO, prod.NOME FROM PRODUTO prod WHERE prod.IDPRODUTO IN (
+  SELECT pi.IDPRODUTO FROM PEDIDO p
+  JOIN PEDIDOITEM pi ON p.IDPEDIDO = pi.IDPEDIDO 
+  AND p.DATAPEDIDO BETWEEN ADD_MONTHS(TRUNC(SYSDATE,'mm'), -4) 
+  AND LAST_DAY(ADD_MONTHS(TRUNC(SYSDATE,'mm'), -4))
+);
+
+-- 2) Altere o status dos produtos (campo situacao) que não tiveram nenhum pedido nos últimos quatro meses.
+UPDATE PRODUTO prod SET prod.SITUACAO = 'I' WHERE prod.IDPRODUTO IN (
+  SELECT pi.IDPRODUTO FROM PEDIDO p
+  JOIN PEDIDOITEM pi ON p.IDPEDIDO = pi.IDPEDIDO 
+  AND p.DATAPEDIDO BETWEEN ADD_MONTHS(TRUNC(SYSDATE,'mm'), -4) 
+  AND LAST_DAY(ADD_MONTHS(TRUNC(SYSDATE,'mm'), -4))
+);
+/* 3) Faça uma consulta que receba um parâmetro sendo o IDProduto e liste a quantidade de itens na tabela 
+      PedidoItem com este IDProduto foram vendidos no último ano (desde janeiro/2017). */
+       
+      
+      
+      
