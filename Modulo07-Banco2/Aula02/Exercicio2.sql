079360126a51cf321dd2793ce2c2eb200694dcea
JabelCwi <jabel.fontoura@cwi.com.br>
2017-06-13 20:21:13 -0300

    feat(lista2): comandos do exercicio 02

--- Modulo07-Banco2/Aula02/Exercicio2.sql
+++ Modulo07-Banco2/Aula02/Exercicio2.sql
@@ -1,3 +1,19 @@
 /* 2)Faça uma rotina que permita atualizar o valor do pedido a partir dos seus itens.
 Esta rotina deve receber por parametro o IDPedido e então verificar o valor total de seus itens (quantidade x valor unitário).
 */
+DECLARE
+  CURSOR C_Pedido(Id IN number) IS
+     SELECT Quantidade, PrecoUnitario FROM PedidoItem
+     WHERE IdPedido = Id;
+   vPedidoId Pedido.IdPedido%TYPE;
+   vValor Pedido.ValorPedido%TYPE;
+ BEGIN
+   vPedidoId := 7;
+   vValor := 0.0;
+   FOR i IN C_Pedido(vPedidoId) LOOP
+     vValor := vValor + ( i.quantidade * i.precounitario);
+   END LOOP;
+   UPDATE Pedido 
+   SET ValorPedido =  vValor
+   WHERE IdPedido = vPedidoId;
+ END;
