a0ebaa3d6aa784c26aae180d5bbc131a56261d57
Jabel <jabel.fontoura@cwi.com.br>
2017-06-14 14:17:28 -0400

    refactor(Exercicio1): ajuste de uf

--- Modulo07-Banco2/Aula02/Exercicio1.sql
+++ Modulo07-Banco2/Aula02/Exercicio1.sql
@@ -1,21 +1,23 @@
-/* 1) Atualmente a tabela de Cidade tem registros duplicados (nome e UF). Faça um código (PL/SQL) que liste quais são as cidades duplicadas. 
-Após isso liste todos os clientes que estão relacionados com estas cidades
+/* 1) Atualmente a tabela de Cidade tem registros duplicados (nome e UF). Faï¿½a um cï¿½digo (PL/SQL) que liste quais sï¿½o as cidades duplicadas. 
+Apï¿½s isso liste todos os clientes que estï¿½o relacionados com estas cidades
 */
 DECLARE
  CURSOR C_ListaCidades IS
      SELECT COUNT (*), Nome, UF FROM CIDADE 
     GROUP BY Nome, UF HAVING COUNT(*) > 1 ;
- CURSOR C_ClientesPorCidades(cidadeAtual in string) IS
+ CURSOR C_ClientesPorCidades(cidadeAtual in string, UFAtual in string) IS
   SELECT cli.Nome AS NomeCliente, cid.Nome AS NomeCidade FROM Cliente cli JOIN Cidade cid ON cli.IDCIDADE = cid.IDCIDADE
-  WHERE cid.Nome LIKE cidadeAtual;
+  WHERE cid.Nome = cidadeAtual AND cid.UF = UFAtual;
   
 BEGIN
   DBMS_OUTPUT.PUT_LINE('---Cidades duplicadas');
    FOR reg IN C_ListaCidades LOOP
-    DBMS_OUTPUT.PUT_LINE('--------- Cidade' || reg.nome || '---------');
-    FOR item  in C_ClientesPorCidades(reg.nome) LOOP
+    DBMS_OUTPUT.PUT_LINE('--------- Cidade' || reg.nome || ' - ' || reg.uf || '---------');
+    FOR item  in C_ClientesPorCidades(reg.nome, reg.uf) LOOP
       DBMS_OUTPUT.PUT_LINE('Cliente -- ' || item.nomeCliente);
      END LOOP;
    END LOOP;
 END;
 
+--CREATE INDEX IX_Cidade_NomeUF ON Cidadde (Nome, UF);
+--CREATE INDEX IX_CLiente_Cidade ON Cliente (IDCidade);
