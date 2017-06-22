23d1ddf1ef008eb8ce77196fb614b0f1e0f5dcde
JabelCwi <jabel.fontoura@cwi.com.br>
2017-06-21 21:25:42 -0300

    feat(exercicios): implemetacao do ReaderUtils

--- Modulo08-Java/aula02/src/main/java/br/com/crescer/tema/Main.java
+++ Modulo08-Java/aula02/src/main/java/br/com/crescer/tema/Main.java
@@ -6,9 +6,13 @@ public class Main {
   
   public static void main(String[] args) {
     // Exercicio 1;
-    FileUtilsImpl utils = new FileUtilsImpl();
+//    FileUtilsImpl utils = new FileUtilsImpl();
+//    
+//    System.out.println(utils.mv("D:\\Classes\\CWI\\testes\\novoArquivo.txt", "D:\\Classes\\CWI\\novoArquivo.txt"));
+
+    // Exercicio 2
     
-    System.out.println(utils.mv("D:\\Classes\\CWI\\testes\\novoArquivo.txt", "D:\\Classes\\CWI\\novoArquivo.txt"));
+    System.out.println(new ReaderUtilsImpl().read("D:\\Classes\\CWI\\testes\\novoArquivo.txt"));
    
   }
 }
---
+++ Modulo08-Java/aula02/src/main/java/br/com/crescer/tema/ReaderUtilsImpl.java
@@ -0,0 +1,37 @@
+package br.com.crescer.tema;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ReaderUtilsImpl implements ReaderUtils{
+
+  private File file;
+  
+  @Override
+  public String read(String string) {
+    try {
+      file = new File(string);
+      
+      if(!string.contains(".txt")) throw new Exception("Este não é um arquivo .txt");
+      else if(!Files.exists(file.toPath())) throw new Exception("Arquivo não encontrado.");
+      
+      final Reader reader = new FileReader(string);
+      final BufferedReader bufferReader = new BufferedReader(reader);
+      
+      final StringBuilder sb = new StringBuilder();
+      
+      bufferReader.lines().forEach(l -> sb.append(l + "\n"));
+      
+      return sb.toString();
+      
+    } catch (Exception ex) {
+      Logger.getLogger(ReaderUtilsImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }
+  }
+}
