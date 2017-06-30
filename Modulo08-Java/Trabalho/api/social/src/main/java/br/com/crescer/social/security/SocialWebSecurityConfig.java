1cbb870d869bd5329991325529793b8fcfdeae07
JabelCwi <jabel.fontoura@cwi.com.br>
2017-06-30 02:13:41 -0300

    feat(cors): configurar acessos do angular

--- Modulo08-Java/Trabalho/api/social/src/main/java/br/com/crescer/social/security/SocialWebSecurityConfig.java
+++ Modulo08-Java/Trabalho/api/social/src/main/java/br/com/crescer/social/security/SocialWebSecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.crescer.social.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import static org.springframework.http.HttpMethod.GET;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,6 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * @author carloshenrique
@@ -34,12 +38,19 @@ public class SocialWebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .logout().logoutUrl("/logout").deleteCookies("JSESSIONID").permitAll()
             .and()
-            .cors().disable()
+            .cors().and()
             .csrf().disable();
   }
 
   @Autowired
   public void setDetailsService(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    return source;
   }
 }
